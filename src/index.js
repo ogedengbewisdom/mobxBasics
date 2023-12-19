953151319615087610e4db2bae1ea030654affe6
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-12-19 21:58:07 +0100

    add todo store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import counterStore from './store';
 import './index.css';
 import App from './App';
 import { Provider } from 'mobx-react';
+import todoStore from './store/todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider counterStore={counterStore}><App /></Provider>);
+root.render(<Provider counterStore={counterStore} todoStore={todoStore}><App /></Provider>);
