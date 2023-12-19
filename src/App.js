2b4fecd0b0f9519af0a4b7f24c39de086a2b3763
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-12-19 21:57:44 +0100

    root app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Auth from './components/Auth';
 import { observer } from 'mobx-react';
 import counterStore from './store';
 import UserProfile from "./components/UserProfile"
+import NewTodos from './components/NewTodos';
+import TodoItem from './components/TodoItem';
 
 
 const App = observer(() => {
@@ -13,8 +15,11 @@ const App = observer(() => {
   return (
     <>
       <Header />
-      {!storeObj.isAuth && <Auth />}
+      {/* {!storeObj.isAuth && <Auth />} */}
       {storeObj.isAuth && <UserProfile />}
+      <TodoItem />
+      <NewTodos />
+     
     {storeObj.isAuth && <Counter />}
     </>
   );
